949b11a1fd4ed0024ac71605facc38aac69e58ac
Saw Paing <sawp2489@gmail.com>
2024-10-04 16:12:28 -0500

    Completed Activity 5

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/User'
+
 
 const app = express();
 
+
 connectDatabase();
+
 
 app.use(express.json({ extended: false }));
 app.use(cors({ origin: 'http://localhost:3000'}));
 
+
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
+
 
 app.post('/api/users',
   [
@@ -25,12 +34,51 @@ app.post('/api/users',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     } else {
-      return res.send(req.body);
+      const { name, email, password } = req.body;
+      try {
+        let user = await User.findOne({ email: email });
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'User already exists'}] });
+        }
+
+        user = new User({
+          name: name,
+          email: email,
+          password: password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '10hr' },
+          (err, token) => {
+            if (err) {
+              throw (err);              
+            }
+            res.json({ token: token });
+          }
+        );
+      } catch (error) {
+        res.status(500).send('Server error');
+      }
     }
 });
 

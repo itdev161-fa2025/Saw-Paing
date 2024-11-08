949af6f0689241f922c3f2752e5c49e5f23f5a55
Saw Paing <sawp2489@gmail.com>
2024-11-08 17:02:02 -0600

    Completed Activity 9

--- server.js
+++ server.js
@@ -6,23 +6,19 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import Post from './models/Post';
 import auth from './middleware/auth';
-
 
 const app = express();
 
-
 connectDatabase();
-
 
 app.use(express.json({ extended: false }));
 app.use(cors({ origin: 'http://localhost:3000'}));
 
-
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
-
 
 app.post('/api/users',
   [
@@ -59,24 +55,8 @@ app.post('/api/users',
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-
-        const payload = {
-          user: {
-            id: user.id
-          }
-        }
-
-        jwt.sign(
-          payload,
-          config.get('jwtSecret'),
-          { expiresIn: '10hr' },
-          (err, token) => {
-            if (err) {
-              throw (err);              
-            }
-            res.json({ token: token });
-          }
-        );
+        
+        returnToken(user, res);
       } catch (error) {
         res.status(500).send('Server error');
       }
@@ -105,7 +85,7 @@ app.post(
     } else {
       const { email, password } = req.body;
       try {
-        
+
         let user = await User.findOne({ email: email });
         if (!user) {
           return res
@@ -113,7 +93,6 @@ app.post(
             .json({ errors: [{ msg: 'Invalid email or password' }] });
         }
 
-        
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
           return res
@@ -121,7 +100,6 @@ app.post(
             .json({ errors: [{ msg: 'Invalid email or password' }] });
         }
 
-        
         returnToken(user, res);
       } catch (error) {
         res.status(500).send('Server error');
@@ -129,6 +107,136 @@ app.post(
     }
   }
 );
+
+const returnToken = (user, res) => {
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
+
+  jwt.sign(
+    payload,
+    config.get('jwtSecret'),
+    { expiresIn: '10hr' },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token: token });
+    }
+  );
+};
+
+app.post(
+  '/api/posts',
+  [
+    auth,
+    [
+      check('title', 'Title text is required')
+        .not()
+        .isEmpty(),
+      check('body', 'Body text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, body } = req.body;
+      try {
+
+        const user = await User.findById(req.user.id);
+
+        const post = new Post({
+          user: user.id,
+          title: title,
+          body: body
+        });
+
+        await post.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
+
+app.get('/api/posts', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.get('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.delete('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
+
+app.put('/api/posts/:id', auth, async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    post.title = title || post.title;
+    post.body = body || post.body;
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
+});
 
 const port = 3001;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
